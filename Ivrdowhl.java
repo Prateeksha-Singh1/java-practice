c8aadf0d559ff5e8755acc0b7de551f3d4fab5e3
Prateeksha-Singh1 <prateekshas054@gmail.com>
2026-03-20 23:27:24 +0530

    Add files via upload

---
+++ Ivrdowhl.java
@@ -0,0 +1,16 @@
+public class Ivrdowhl {
+     public static void main(String[] args) {
+        int i = 5;
+
+        do {
+            int j = 1;
+            do {
+                System.out.print("* ");
+                j++;
+            } while (j <= i);
+
+            System.out.println();
+            i--;
+        } while (i >= 1);
+    }
+}
