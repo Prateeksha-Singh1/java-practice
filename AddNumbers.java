e2a3f3a5e59f20769570eb8c2186550edeee06ee
Prateeksha-Singh1 <prateekshas054@gmail.com>
2026-03-23 00:02:32 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,10 @@
+public class AddNumbers {
+    public static void add(int a, int b) {
+        System.out.println("Sum: " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        add(5, 10);
+    }
+    
+}
---
+++ ReturnExample.java
@@ -0,0 +1,11 @@
+public class ReturnExample {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int result = multiply(4, 5);
+        System.out.println("Result: " + result);
+    }
+    
+}
